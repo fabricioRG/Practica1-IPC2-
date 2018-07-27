02216edce7b3d6a2776dfd4ce9408b6c50c08bcf
Fabricio R <fabricio21rg@gmail.com>
2018-07-27 13:02:35 -0600

    Primer commit de Practica 1
    Se inicializo el repositorio llamado 'Practica1(IPC2)

---
+++ src/calculadora/Calculadora.java
@@ -0,0 +1,17 @@
+package calculadora;
+import Interfaz.CalculadoraInterfaz;
+/**
+ *
+ * @author fabricio
+ */
+public class Calculadora {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        CalculadoraInterfaz ci = new CalculadoraInterfaz();
+        ci.setVisible(true);
+    }
+    
+}
